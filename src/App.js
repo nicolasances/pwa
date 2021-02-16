a7dc3a415595e5cc9279d67d7cf8fb4fad9f0c3d
Nicolas Matteazzi <NIMAT@orsted.dk>
2021-02-16 20:19:55 +0100

    Initial import

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
-function App() {
+import googleLogo from './img/google-logo.png';
+
+function HomeScreen() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="screen">
+      <br/>
+      <Link to='/hello'>Want to say hello?</Link>
+      This is the home screen
     </div>
-  );
+  )
+}
+
+function HelloScreen() {
+  return (
+    <div class="screen">
+      <br/>
+      Hello! Welcome to this stupid app!!
+      <Link to='/'>Go back out!!</Link>
+    </div>
+  )
+}
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    }
+
+  }
+
+  render() {
+
+    // Define the content  based on the sign in state
+    let content = (
+      <div className="toto-app">
+        Hello there!
+        <Switch>
+          <Route exact path='/' component={HomeScreen} />
+          <Route exact path='/hello' component={HelloScreen} />
+        </Switch>
+      </div>
+    )
+
+
+    return (
+      <BrowserRouter basename="/">
+        {content}
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
